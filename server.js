761e82fe65a4ec05b1bb5afaf2297b18632c267c
sam hockaday <samuel.hockaday@gmail.com>
2024-03-11 21:00:30 +0000

    Basic styling done. Next is to add Back end GPT

--- server.js
+++ server.js
@@ -4,9 +4,29 @@ require('dotenv').config();
 const {configuration, OpenAI } = require ('openai');
 const app = express();
 
+// const openai = new OpenAI({
+//     apiKey: process.env.openaiAPI,
+// });
+
+app.get("/openai", async (req, res) => {
+    const completion = await openai.chat.completions.create({
+        messages: [
+            {
+                role: "user",
+                content: `Create a recipe`
+            },
+        ],
+        model: "gpt-3.5-turbo",
+        max_tokens: 2000,
+    });
+    res.json(completion);
+     console.log(completion);
+});
+
 // app.get('/', (req, res)=> {
 //     res.send('welcome to my server!');
 // });
+
 app.use(express.static('Public')); 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
