6440eff0eebe30b1e426c42bdbecd38e64812d9b
josueJURE <josue.jure@gmail.com>
2024-04-03 15:32:24 +0100

    feat(JS): add logic to eventListener() on darkLightButton element

--- public/index.js
+++ public/index.js
@@ -1,15 +1,25 @@
 const resultElement = document.querySelector(".gpt-response");
 const buttons = document.querySelectorAll("button");
 const darkLightButton = document.querySelector(".dark-light-button");
-
-
+const allergies = document.querySelector(".allergies");
+const title = document.querySelector("h1");
 
 darkLightButton.addEventListener("change", () => {
   console.log(darkLightButton.checked)
+  if(darkLightButton.checked) {
+    allergies.style.setProperty('--green', 'black');
+    title.style.setProperty('--green', 'black');
+    buttons.forEach(button => {
+      button.style.setProperty('--green', 'black')
+    })
+  } else {
+    allergies.style.setProperty('--green', 'rgb(183, 235, 183)')
+    title.style.setProperty('--green', 'rgb(183, 235, 183)');
+    buttons.forEach(button => {
+      button.style.setProperty('--green', 'rgb(183, 235, 183)');
+    })
+  }
 })
-
-
-
 
 
 let recipeName;
