b6f5bd2034a43bd8b941b1de6fdf116fc3205d78
josueJURE <josue.jure@gmail.com>
2024-04-03 14:47:06 +0100

    feat(JS): add eventListener() to darkLightButton

--- public/index.js
+++ public/index.js
@@ -1,6 +1,12 @@
 const resultElement = document.querySelector(".gpt-response");
 const buttons = document.querySelectorAll("button");
-const darkLightButton = document.querySelectorAll(".dark-light-button");
+const darkLightButton = document.querySelector(".dark-light-button");
+
+
+
+darkLightButton.addEventListener("change", () => {
+  console.log(darkLightButton.checked)
+})
 
 
 
