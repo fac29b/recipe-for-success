cfdd98c18f4f64cf560511b0afc8f362c23a585a
josueJURE <josue.jure@gmail.com>
2024-04-08 13:46:37 +0100

    feat(JS): declutter and format codebase

--- public/index.js
+++ public/index.js
@@ -1,99 +1,52 @@
-›// const { application } = require("express");
-
-
 const resultElement = document.querySelector(".gpt-response");
 const buttons = document.querySelectorAll("button");
 const lactoseIntolerant = document.querySelector(".lactose-intolerant");
 let isLactoseIntolerant;
 let recipeName;
 
-// lactoseIntolerant.addEventListener("change", () => {
-//   if(recipeName === "") return
-//   isLactoseIntolerant = lactoseIntolerant.checked
-//   console.log(isLactoseIntolerant)
- 
-//   fetch("public/server.js", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type" : "application/json"
-//     },
-//     body: JSON.stringify({variable2: isLactoseIntolerant})
-//   })
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log("Response from the back-end about lactose intolerant", data)
-//   })
-//   .catch(error => {
-//     console.error("Error", error)
-//   })
-//   fetch(`/openai?lactose=${encodeURIComponent(isLactoseIntolerant)}`)
-//   .then(response => response.json())
-//   .then(data => { 
-//     if (resultElement) {
-//         resultElement.innerHTML = `<p>${data.choices[0].message.content}</p>`;
-//     } else {
-//         console.error('Error: Element with class "gpt-response" not found');
-//     }
-// })
-// .catch(error => console.error('Error:', error));
-// })
+lactoseIntolerant.addEventListener("click", () => {
+  console.log(lactoseIntolerant.checked);
+});
 
-lactoseIntolerant.addEventListener("click", ()=> {
-  console.log(lactoseIntolerant.checked)
-
-})
-
-
-
-
-
-
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-      console.log(lactoseIntolerant.checked)
-      console.log("hello world")
-      recipeName = button.innerHTML;
-      fetch("public/server.js", {
-        method: "POST",
-        headers: {
-            "Content-Type" : "application/json"
-        }, 
-        body: JSON.stringify({
-          variable: recipeName,
-          variable2 : lactoseIntolerant.checked
-        })
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log("Response from the back-end", data)
-      })
-      .catch(error => {
-        console.error("Error", error)
-      })
-      JSON.stringify({
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(lactoseIntolerant.checked);
+    console.log("hello world");
+    recipeName = button.innerHTML;
+    fetch("public/server.js", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
         variable: recipeName,
-        variable2 : lactoseIntolerant.checked
-      })
-   
-
-    
-    
-      fetch(`/openai?recipe=${encodeURIComponent(recipeName)}&lactose=${encodeURIComponent(lactoseIntolerant.checked)} `)
-      .then(response => response.json())
-      .then(data => { 
-          if (resultElement) {
-              resultElement.innerHTML = `<p>${data.choices[0].message.content}</p>`;
-          } else {
-              console.error('Error: Element with class "gpt-response" not found');
-          }
-      })
-      .catch(error => console.error('Error:', error));
+        variable2: lactoseIntolerant.checked,
+      }),
     })
-  })
-
-
-
-
-
-
-
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Response from the back-end", data);
+      })
+      .catch((error) => {
+        console.error("Error", error);
+      });
+    JSON.stringify({
+      variable: recipeName,
+      variable2: lactoseIntolerant.checked,
+    });
+    fetch(
+      `/openai?recipe=${encodeURIComponent(
+        recipeName
+      )}&lactose=${encodeURIComponent(lactoseIntolerant.checked)} `
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (resultElement) {
+          resultElement.innerHTML = `<p>${data.choices[0].message.content}</p>`;
+        } else {
+          console.error('Error: Element with class "gpt-response" not found');
+        }
+      })
+      .catch((error) => console.error("Error:", error));
+  });
+});
--- server.js
+++ server.js
@@ -6,32 +6,33 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.post("/public/server.js", (req, res) => {
   const variableFromFrontEnd = req.body.variable;
-  const variableFromFrontEnd2= req.body.variable2
-  console.log("Received variable from the front-end:", variableFromFrontEnd);
-  console.log("Received boolean from front-end:", variableFromFrontEnd2);
-  res.json({ message: "Variable received successfully" });
+  const variableFromFrontEnd2 = req.body.variable2;
+  res.json({
+    message: `Variables ${variableFromFrontEnd} and ${variableFromFrontEnd2} received successfully`,
+  });
 });
 const openai = new OpenAI({
   apiKey: process.env.openaiAPI,
 });
 app.get("/openai", async (req, res) => {
   const recipeFromFrontEnd = req.query.recipe;
-  const isLactoseIntolerant = req.query.lactose
-  console.log({isLactoseIntolerant}, {recipeFromFrontEnd})
-
+  const isLactoseIntolerant = req.query.lactose;
+  console.log({ isLactoseIntolerant }, { recipeFromFrontEnd });
 
   const completion = await openai.chat.completions.create({
     messages: [
       {
         role: "user",
-        content: `Provide a recipe for ${recipeFromFrontEnd} the user is lactose intolerant ${isLactoseIntolerant === false ? "no" : "yes"}`,
+        content: `Provide a recipe for ${recipeFromFrontEnd} the user is lactose intolerant ${
+          isLactoseIntolerant === false ? "no" : "yes"
+        }`,
       },
     ],
     model: "gpt-3.5-turbo",
     max_tokens: 2000,
   });
   res.json(completion);
-  console.log({lactoseBoolean: isLactoseIntolerant}, {recipeFromFrontEnd})
+  console.log({ lactoseBoolean: isLactoseIntolerant }, { recipeFromFrontEnd });
 });
 app.use(express.static("public"));
 const port = process.env.PORT || 3000;
