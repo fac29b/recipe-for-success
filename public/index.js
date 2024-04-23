73c34bc6dbff5487a9d2d25a08f9381969a2bf59
josueJURE <josue.jure@gmail.com>
2024-04-23 13:19:41 +0100

    feat(JS) implement logic to toggle the display of the buttons

--- public/index.js
+++ public/index.js
@@ -7,8 +7,8 @@ const loadingContainer = document.querySelector("#loading-container");
 const allergies = document.querySelector(".allergies");
 const darkLightButton = document.querySelector(".dark-light-button");
 const userWantAnotherRecipe = document.querySelector(".want-another-recipe");
-console.log(userWantAnotherRecipe)
-const buttons = document.querySelectorAll("button");
+const recipeButtons = document.querySelectorAll(".recipe-button");
+console.log(recipeButtons);
 const dietaryRequirements = Array.from(
   document.querySelectorAll(".dietary-requirements")
 );
@@ -16,19 +16,33 @@ let isLactoseIntolerant;
 let dishOriginCountry;
 
 
+console.log(recipeButtons)
+
+
+userWantAnotherRecipe.addEventListener("click", () => {
+  [ headline, allergies, ...recipeButtons].forEach(elememt => {
+    elememt.style.display = "block";
+    [ gptResponseElement, userWantAnotherRecipe].forEach(elememt => {
+      elememt.style.display = "none"
+    })
+    gptResponseElement.innerHTML = ""
+  })
+})
+
+
 
 darkLightButton.addEventListener("change", () => {
   let color = darkLightButton.checked
     ? "rgb(67, 63, 63)"
     : "rgb(183, 235, 183)";
-  [gptResponseElement, lactoseIntolerant, allergies, headline, ...buttons].forEach(
+  [gptResponseElement, lactoseIntolerant, allergies, headline, ...recipeButtons].forEach(
     (element) => {
       element.style.setProperty("--green", color);
       element.style.transition = "background-color 0.5s ease";
     }
   );
 });
-buttons.forEach((button) => {
+recipeButtons.forEach((button) => {
   button.addEventListener("click", () => {
     let userRecipe = {
       [button.name]: button.value,
@@ -75,9 +89,10 @@ buttons.forEach((button) => {
           const imageUrl = data.image.data[0].url;
           mainElement.style.backgroundImage = `url(${imageUrl})`
           gptResponseElement.innerHTML = `${textContent}`;
-            [ headline, allergies, ...buttons].forEach(elememt => {
+            [ headline, allergies, ...recipeButtons].forEach(elememt => {
               elememt.style.display = "none"
             })
+            userWantAnotherRecipe.style.display = "block";
         } else {
           console.error('Error: Element with class "gpt-response" not found');
         }
