ef46ec48b993106ee0838efd2f878ab0752872a3
Josue JURE <57095041+josueJURE@users.noreply.github.com>
2024-05-29 14:55:09 +0100

    (feat:JS) create a run object

--- server.js
+++ server.js
@@ -120,6 +120,32 @@ app.get("/openai", async (req, res) => {
     }
   );
 
+  // We use the stream SDK helper to create a run with
+// streaming. The SDK provides helpful event listeners to handle 
+// the streamed response.
+ 
+const run = openai.beta.threads.runs.stream(thread.id, {
+  assistant_id: assistant.id
+})
+  .on('textCreated', (text) => process.stdout.write('\nassistant > '))
+  .on('textDelta', (textDelta, snapshot) => process.stdout.write(textDelta.value))
+  .on('toolCallCreated', (toolCall) => process.stdout.write(`\nassistant > ${toolCall.type}\n\n`))
+  .on('toolCallDelta', (toolCallDelta, snapshot) => {
+    if (toolCallDelta.type === 'code_interpreter') {
+      if (toolCallDelta.code_interpreter.input) {
+        process.stdout.write(toolCallDelta.code_interpreter.input);
+      }
+      if (toolCallDelta.code_interpreter.outputs) {
+        process.stdout.write("\noutput >\n");
+        toolCallDelta.code_interpreter.outputs.forEach(output => {
+          if (output.type === "logs") {
+            process.stdout.write(`\n${output.logs}\n`);
+          }
+        });
+      }
+    }
+  });
+
  
 
 
