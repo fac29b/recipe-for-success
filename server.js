8b9ac3530ad351cae7d92f9de64de433ec8616f3
Josue JURE <57095041+josueJURE@users.noreply.github.com>
2024-06-15 06:52:27 +0100

    (feat:JS): format code

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
@@ -7,143 +6,125 @@ const { OpenAI } = require("openai");
 const app = express();
 const bodyParser = require("body-parser");
 
-
 require("dotenv").config();
 
 app.use(bodyParser.json());
-
 
 const openai = new OpenAI({
   apiKey: process.env.openaiAPI,
 });
 
-
 let recipe = "";
 let url;
 
-
-
 app.get("/stream", async (req, res) => {
   try {
-  res.setHeader("Content-Type", "text/event-stream");
-  res.setHeader("Cache-Control", "no-cache");
-  res.setHeader("Connection", "keep-alive");
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Connection", "keep-alive");
 
-  const {
-    recipe_country_of_origin,
-    is_lactose_intolerant,
-    is_vegan,
-    what_are_user_other_dietary_requirements,
-  } = req.query;
+    const {
+      recipe_country_of_origin,
+      is_lactose_intolerant,
+      is_vegan,
+      what_are_user_other_dietary_requirements,
+    } = req.query;
 
+    let prompt = `Provide a recipe for a dish from ${recipe_country_of_origin}, taking into account the fact that I'm ${
+      is_lactose_intolerant === "true"
+        ? "lactose intolerant"
+        : "not lactose intolerant"
+    } ${is_vegan === "true" ? "vegan" : "not vegan"} and ${
+      what_are_user_other_dietary_requirements === ""
+        ? "I have no other dietary requirements"
+        : what_are_user_other_dietary_requirements
+    } `;
 
+    console.log({ streamPrompt: prompt });
 
-  let prompt = `Provide a recipe for a dish from ${recipe_country_of_origin}, taking into account the fact that I'm ${
-    is_lactose_intolerant === "true"
-      ? "lactose intolerant"
-      : "not lactose intolerant"
-  } ${is_vegan === "true" ? "vegan" : "not vegan"} and ${
-    what_are_user_other_dietary_requirements === ""
-      ? "I have no other dietary requirements"
-      : what_are_user_other_dietary_requirements
-  } `;
-
-  console.log({ streamPrompt: prompt });
-
-  const stream = await openai.chat.completions.create({
-    messages: [
-      {
-        role: "user",
-        content: `${prompt}`,
-      },
-    ],
-    model: "gpt-3.5-turbo",
-    max_tokens: 500,
-    stream: true,
-  });
-
-
-
-  for await (const chunk of stream) {
-    const finishReason = chunk.choices[0].finish_reason;
-
-   
-
-    if (finishReason === "stop") {
-      console.log("finished generating recipe");
-      console.log(`recipie=${recipe}`);
-      break;
-    }
-
-    if (typeof recipe_country_of_origin === "undefined") {
-      res.status(400).send("Missing recipe_country_of_origin query parameter");
-      return;
-    } else {
-      const message = chunk.choices[0]?.delta?.content || "";
-      recipe += message;
-      messageJSON = JSON.stringify({ message });
-      res.write(`data: ${messageJSON}\n\n`); // Send the message to the client
-    }
-  }
-
-  const imagePromise = openai.images
-    .generate({
-      model: "dall-e-3",
-      prompt: `${recipe}`,
-      n: 1,
-      size: "1024x1024",
-    })
-    .then((image) => {
-      console.log(image)
-      messageJSON = JSON.stringify({ image });
-      res.write(`data: ${messageJSON}\n\n`); // Send the message to the client
-      const folderPath = path.join(__dirname, "./public/url_folder");
-      url = image.data[0].url;
-      const filePath = path.join(folderPath, "url_folder.txt");
-      fs.writeFileSync(filePath, url);
+    const stream = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "user",
+          content: `${prompt}`,
+        },
+      ],
+      model: "gpt-3.5-turbo",
+      max_tokens: 500,
+      stream: true,
     });
 
-  const audioPromise = openai.audio.speech
-    .create({
-      model: "tts-1",
-      voice: "alloy",
-      input: `${recipe}`,
-    })
-    .then(async (mp3) => {
-      const speechFile = path.resolve("./speech.mp3");
-      console.log(speechFile);
-      const buffer = Buffer.from(await mp3.arrayBuffer());
-      await fs.promises.writeFile(speechFile, buffer);
-      messageJSON = JSON.stringify({ audio: buffer.toString("base64") });
-      res.write(`data: ${messageJSON}\n\n`); // Send the message to the client
-    });
+    for await (const chunk of stream) {
+      const finishReason = chunk.choices[0].finish_reason;
+
+      if (finishReason === "stop") {
+        console.log("finished generating recipe");
+        console.log(`recipie=${recipe}`);
+        break;
+      }
+
+      if (typeof recipe_country_of_origin === "undefined") {
+        res
+          .status(400)
+          .send("Missing recipe_country_of_origin query parameter");
+        return;
+      } else {
+        const message = chunk.choices[0]?.delta?.content || "";
+        recipe += message;
+        messageJSON = JSON.stringify({ message });
+        res.write(`data: ${messageJSON}\n\n`);
+      }
+    }
+
+    const imagePromise = openai.images
+      .generate({
+        model: "dall-e-3",
+        prompt: `${recipe}`,
+        n: 1,
+        size: "1024x1024",
+      })
+      .then((image) => {
+        console.log(image);
+        messageJSON = JSON.stringify({ image });
+        res.write(`data: ${messageJSON}\n\n`);
+        const folderPath = path.join(__dirname, "./public/url_folder");
+        url = image.data[0].url;
+        const filePath = path.join(folderPath, "url_folder.txt");
+        fs.writeFileSync(filePath, url);
+      });
+
+    const audioPromise = openai.audio.speech
+      .create({
+        model: "tts-1",
+        voice: "alloy",
+        input: `${recipe}`,
+      })
+      .then(async (mp3) => {
+        const speechFile = path.resolve("./speech.mp3");
+        console.log(speechFile);
+        const buffer = Buffer.from(await mp3.arrayBuffer());
+        await fs.promises.writeFile(speechFile, buffer);
+        messageJSON = JSON.stringify({ audio: buffer.toString("base64") });
+        res.write(`data: ${messageJSON}\n\n`);
+      });
 
     return Promise.all([imagePromise, audioPromise]).then(() => {
       messageJSON = JSON.stringify({ message: "stop" });
-      res.write(`data: ${messageJSON}\n\n`); // Send the message to the client
+      res.write(`data: ${messageJSON}\n\n`);
       res.end();
     });
+  } catch (error) {
+    if (error.code === "invalid_api_key") {
+      let errorMessage = error.code;
+      let errorJSON = JSON.stringify({ errorMessage });
+      res.write(`data: ${errorJSON}\n\n`);
+      console.error("Invalid API key provided. Please check your API key.");
+    } else {
+      console.error("An error occurred:", error.message);
+    }
 
-
-} catch (error) {
-  if (error.code === 'invalid_api_key') {
-    let errorMessage = error.code;
-    let errorJSON = JSON.stringify({errorMessage})
-    res.write(`data: ${errorJSON}\n\n`) // send the message to the client
-    console.error('Invalid API key provided. Please check your API key.');
-
-    // res.status(401).json({ error: 'Invalid API key provided. Please check your API key.' });
-  
-
-  } else {
-    console.error("An error occurred:", error.message);
-    // res.status(500).json({ error: error.message });
-   
+    console.log("error");
   }
-
-  console.log("error")
-}
-
 });
 
 app.get("/email", async (req, res) => {
@@ -154,7 +135,6 @@ app.get("/email", async (req, res) => {
       pass: process.env.third_party_app_password,
     },
   });
-
 
   const emailDocument = `
   <html>
@@ -193,12 +173,10 @@ app.get("/email", async (req, res) => {
           cid: "url",
         },
       ],
-      
     };
   } else {
     console.log("doubleResponse is not defined yet.");
   }
-
 
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
